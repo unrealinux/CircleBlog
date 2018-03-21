09dedf8e4268e904594925022f32fffe7e2cb149
unrealinux <121058689@qq.com>
2018-03-21 23:43:40 +0800

    Django初始化博客工程

---
+++ CircleBlog/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CircleBlog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CircleBlog.settings")
+
+application = get_wsgi_application()
